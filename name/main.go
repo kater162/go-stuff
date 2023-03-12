249c997c86421e1af0ceba1d1e15f043f4fe65d1
Kater <kater@ktrtechlabs.com>
2023-03-12 18:17:54 +1100

    added name program

---
+++ name/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	fmt.Println("What is your name?")
+	reader := bufio.NewReader(os.Stdin)
+	name, err :=  reader.ReadString('\n')
+	if err == nil {
+		fmt.Println("Hello", name)
+	} else {
+		log.Fatal(err)
+	}
+}
